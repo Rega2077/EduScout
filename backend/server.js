05936d1c5d46b3781ca9b7486f482558b5fb1a1a
suyash <suyashsingh874@gmail.com>
2024-09-19 18:01:32 +0530

    Initial commit: set up frontend and backend with .gitignore

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
